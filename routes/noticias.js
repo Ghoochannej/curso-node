0b3b12c64ac1841a8d4dee1aa77949458331144f
Caio Henrique <caiohenrique6@outlook.com>
2018-05-22 13:21:40 -0300

    bug mysql

--- routes/noticias.js
+++ routes/noticias.js
@@ -7,15 +7,20 @@ module.exports = function(app){
         var connection = mysql.createConnection({
             host :  'localhost',
             user : 'root',
+            port : 3306,
             password : '1234',
             database : 'portal_noticias'
         });
 
+        
+        connection.connect(function(err){
+            if(err) return console.log(err + " /////// fim erro ");
+            console.log('conectou!');
+        })
+        
         connection.query('select * from noticias', function(error, result){
-            res.send(result);
+            res.render("noticias",{noticias : result});
         });
 
-        
-          //  res.render("noticias");
     });
 };
